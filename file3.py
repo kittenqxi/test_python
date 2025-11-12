a8f07e8c914d18ecd3660467e19dbff2e8fc596b
Sonya Pesh <sonypeshevich2007@gmail.com>
2025-11-12 17:46:49 +0300

    add fifth task

--- file3.py
+++ file3.py
@@ -12,9 +12,22 @@
 # for i in range(1, h+1):
 #     print(' ' * (h-i)+ '*'*i)
 #Задание номер четыре
-h=int(input('Enter size of the side: '))
+# h=int(input('Enter size of the side: '))
+# for i in range(h):
+#     if i==0 or i==h-1:
+#         print('*'*h)
+#     else:
+#         print('*'+' '*(h-2)+'*')
+h=int(input('Enter hight: '))
 for i in range(h):
-    if i==0 or i==h-1:
-        print('*'*h)
-    else:
-        print('*'+' '*(h-2)+'*')
+    for j in range(h-i-1):
+        print(' ', end='')
+    for j in range(2*i+1):
+        print('*', end='')
+    print()
+for i in range(h-2, -1, -1):
+    for j in range(h-i-1):
+        print(' ', end='')
+    for j in range(2*i+1):
+        print('*', end='')
+    print()    
