fcb26de3340ff710321641bb2876e8caf9452bc5
Sonya Pesh <sonypeshevich2007@gmail.com>
2025-11-12 17:23:28 +0300

    add second task

--- file3.py
+++ file3.py
@@ -1,5 +1,9 @@
 #Задание номер один
-w=int(input('Enter width: '))
-h=int(input('Enter hight: '))
-for i in range(h):
-    print('*' * w)
+# w=int(input('Enter width: '))
+# h=int(input('Enter hight: '))
+# for i in range(h):
+#     print('*' * w)
+#Задание номер два
+h=int(input('Enter hight'))
+for i in range(1, h+1):
+    print('*' * i)
