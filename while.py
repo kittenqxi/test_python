46eb77383c42df3c033d459459aed491b77e1e7e
igorecc <retrik1990@gmail.com>
2025-11-30 13:32:23 +0300

    add new file to train WHILE

---
+++ while.py
@@ -0,0 +1,22 @@
+# taks 1
+# Ask the user to enter numbers one by one.
+# Keep asking for numbers while the number is positive (greater than 0).
+# Add all positive numbers together.
+# When the user enters a number 0 or negative, stop the loop and print the total sum.
+# Example:
+# User inputs: 5, 3, 2, -1 → program prints sum = 10.
+
+
+# task 2
+# Ask the user to enter a number.
+# Print each digit of the number one by one, starting from the last digit.
+# Example:
+# User types: 123
+# Output should be:
+# 3
+# 2
+# 1
+
+
+
+
